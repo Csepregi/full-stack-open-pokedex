7a9dd5eb078ec6bb9d731cfeebf7486cd8aa2c8c
Gabor Csepregi <gabor.csepregi@accenture.com>
2023-02-06 17:54:41 +0100

    cypress route

--- .cypress.config.js
+++ .cypress.config.js
@@ -4,5 +4,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents() {},
     baseUrl: "http://localhost:8080",
+    specPattern: path.resolve(__dirname, "path/to/src/**/*.cy.js"),
   },
 });
